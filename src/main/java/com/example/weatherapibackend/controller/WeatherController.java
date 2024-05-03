505b42f0452a8a5a268e71648ead555f3ed7a205
Milosz <milek6111@gmail.com>
2024-05-03 13:08:49 +0200

    Parameters checking

--- src/main/java/com/example/weatherapibackend/controller/WeatherController.java
+++ src/main/java/com/example/weatherapibackend/controller/WeatherController.java
@@ -1,7 +1,7 @@
 package com.example.weatherapibackend.controller;
 
 import com.example.weatherapibackend.service.WeatherService;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import io.micrometer.common.lang.NonNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,15 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-
-import java.util.Map;
 
 //test url
 //http://localhost:8080/api/v1/forecast?latitude=1.02&longitude=23.55
@@ -34,7 +25,7 @@ public class WeatherController {
     }
 
     @GetMapping("/forecast")
-    ResponseEntity<?> getForecast(@RequestParam Double latitude,@RequestParam Double longitude){
+    ResponseEntity<?> getForecast(@NonNull @RequestParam Double latitude,@NonNull @RequestParam Double longitude){
         if(latitude < -90 || latitude > 90){
             return new ResponseEntity<>("Latitude must be between -90 and 90",HttpStatus.valueOf(400));
         }
